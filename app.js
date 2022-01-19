1372e62cac941762f39af065b06eb73d323b1c9a
chengyen.chiang <chengyen.chiang@leediancn.com>
2022-01-20 01:31:21 +0800

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const dataBaseInit = require("./lib/db/dataBaseInit");
+const config = require('./lib/setting/config').config;
+const serverPort = config.serverPort;
+const mongoDBPort = config.mongoDBPort;
+const mongoDBName = config.mongoDBName;
+const server = require('http').createServer(app);
+const serverUse=require('./lib/serverUse');
+const userRestApi = require("./lib/rest_api/user");
+
+dataBaseInit.mongoDBInit(mongoDBPort,mongoDBName);
+
+serverUse.on(app);
+userRestApi.on(app);
+
+server.listen(process.env.PORT||serverPort);
+console.log("現在使用" + serverPort + "port");
