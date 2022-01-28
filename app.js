0affb7e43b5f22408ca5c99344293d6e35382d9f
chengyen.chiang <chengyen.chiang@leediancn.com>
2022-01-28 17:42:15 +0800

    add env

--- app.js
+++ app.js
@@ -8,11 +8,9 @@ const mongoDBName = config.mongoDBName;
 const server = require('http').createServer(app);
 const serverUse=require('./lib/serverUse');
 const userRestApi = require("./lib/rest_api/user");
-
+console.log(config)
 dataBaseInit.mongoDBInit(mongoDBPort,mongoDBName);
-
 serverUse.on(app);
 userRestApi.on(app);
-
 server.listen(process.env.PORT||serverPort);
 console.log("現在使用" + serverPort + "port");
