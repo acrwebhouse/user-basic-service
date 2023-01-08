aa542d9bf8956dbf7d4b3ff557ee0c36c868648d
chengyen.chiang <chengyen.chiang@leediancn.com>
2023-01-09 02:10:47 +0800

    db version update

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ const app = express();
 const dataBaseInit = require("./lib/db/dataBaseInit");
 const config = require('./lib/setting/config').config;
 const serverPort = config.serverPort;
-const mongoDBPort = config.mongoDBPort;
 const mongoDBName = config.mongoDBName;
 const server = require('http').createServer(app);
 const serverUse=require('./lib/serverUse');
 const userRestApi = require("./lib/rest_api/user");
 console.log(config)
-dataBaseInit.mongoDBInit(mongoDBPort,mongoDBName);
+dataBaseInit.mongoDBInit(mongoDBName);
 serverUse.on(app);
 userRestApi.on(app);
 server.listen(process.env.PORT||serverPort);
